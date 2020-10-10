f94195f377ed8a1fc2e5fab4ef5340910927c985
Margo Astreyko <margoastreyko@mail.ru>
2020-10-10 16:56:50 +0300

    addingErrorComponent

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {GitService} from './services/';
 import App from './components/app';
+import ErrorBoundry from './components/error-boundary';
 import GitServiceContext from './components/git-service-context';
 import {Provider} from 'react-redux';
 import store from './store';
@@ -12,13 +13,16 @@ import store from './store';
 const gitService=new GitService(); 
 ReactDOM.render(
   <Provider store={store}>
-    
-    <GitServiceContext.Provider value={gitService} >
- 
-       <App />
-  
-    </GitServiceContext.Provider>
+    <ErrorBoundry>
 
+      <GitServiceContext.Provider value={gitService} >
+ 
+        <App />
+  
+      </GitServiceContext.Provider>
+
+
+    </ErrorBoundry>
   </Provider>,
   document.getElementById('root')
 );
