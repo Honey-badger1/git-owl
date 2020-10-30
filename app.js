9a30b39cfe6bda290757c2690705b679a7fef6b0
Margo Astreyko <margoastreyko@mail.ru>
2020-10-30 22:12:21 +0300

    Heroku deployment

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express')
-const config = require('config')
+const config = require('./db');
 const mongoose = require('mongoose')
 
 const app = express()
 
+app.use(express.static("public"))
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'), {
+    await mongoose.connect(process.env.mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
-    })
+    }).then(
+      () => {console.log('Database is connected') },
+      err => { console.log('Can not connect to the database'+ err)}
+  );
+  
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
   } catch (e) {
     console.log('Server Error', e.message)
