18eee2bc195e3add1e2b882caf5c933dff38b1c5
Margo Astreyko <margoastreyko@mail.ru>
2020-10-31 21:29:03 +0300

    changed

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 const express = require('express')
-const config = require('./db');
+const config = require('config')
 const mongoose = require('mongoose')
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const app = express()
 
 app.use(express.static("public"))
+
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
-mongoose.set("debug",true)
-mongoose.Promise=global.Promise
 
-const PORT = process.env.PORT || 5000;
-const db="mongodb+srv://margo:margo12345@gitowl.a7zud.mongodb.net/auth?retryWrites=true&w=majority"
+const PORT = config.get('port') || 5000
+
 async function start() {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
-    }).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
-  );
-  mongoose.connection.on('connected', ()=>{
-    console.log('connected')
-  })
-  
+    })
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
   } catch (e) {
     console.log('Server Error', e.message)
@@ -37,5 +26,4 @@ async function start() {
 }
 
 start()
-
 
