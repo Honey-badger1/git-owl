da9645b670fd4ddc11b96b8f92819f58327a7d0c
Margo Astreyko <margoastreyko@mail.ru>
2020-11-01 00:56:14 +0300

    changed heroku

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ app.use(express.static("public"))
 
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
+mongoose.set("debug", true);
+mongoose.Promise=global.Promise
+
 
 const PORT = process.env.PORT|| 5000
 
 async function start() {
   try {
+   
+    
     await mongoose.connect(process.env.mongoUri||'mongodb://localhost:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
