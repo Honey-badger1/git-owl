7cbf05efe51045207ab261bbe5ce6c4833ce69b1
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-02 15:03:00 +0300

    fix: proxy connection

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,18 +1,18 @@
-// const proxy = require("http-proxy-middleware");
-// module.exports = function(app) {
-//   app.use(proxy("/api/**", {
-//     target: "http://localhost:5000",
-//     secure: false
-//   }));
-// };
-
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
-  app.use(
-    '/API',
-    proxy({
-      target: 'http://localhost:5000',
-      changeOrigin: true
-    })
-  );
+  app.use(proxy("/api/**", {
+    target: "http://localhost:5000",
+    secure: false
+  }));
 };
+
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+//   app.use(
+//     '/API',
+//     proxy({
+//       target: 'http://localhost:5000',
+//       changeOrigin: true
+//     })
+//   );
+// };
