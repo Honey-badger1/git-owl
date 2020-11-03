9dda4823a30c0037f19277e5adc88b0810d54593
Margo Astreyko <margoastreyko@mail.ru>
2020-11-03 21:44:55 +0300

    Another db

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    DB: 'mongodb://margo:margo12345@gitowl-shard-00-00.a7zud.mongodb.net:27017,gitowl-shard-00-01.a7zud.mongodb.net:27017,gitowl-shard-00-02.a7zud.mongodb.net:27017/auth?ssl=true&replicaSet=atlas-2yuga2-shard-0&authSource=admin&retryWrites=true&w=majority'
+    DB: 'mongodb+srv://user:pass1234@cluster0.8fycj.azure.mongodb.net/User?retryWrites=true&w=majority'
 }
