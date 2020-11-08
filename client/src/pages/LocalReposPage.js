83e26ad76f65764d2e0e5ec67d688820059d54cd
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-08 20:23:14 +0300

    feat: add component Button

--- client/src/pages/LocalReposPage.js
+++ client/src/pages/LocalReposPage.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Button from '../components/Button/Button';
 
 export const LocalReposPage = () => {
   return (
     <React.Fragment>
-      <h1>на этой странице мы работаем с локальными репозиториями</h1>
+      <h1>Local Repositories</h1>
+
+      <Button
+        type="primary"
+        onClick={() => console.log('click')}
+      >
+        go to desktop app
+      </Button>
+
+      <Button
+        type="success"
+        onClick={() => console.log('click')}
+      >
+        saved links
+      </Button>
+
+      <Button
+        type="success"
+        onClick={() => console.log('click')}
+      >
+        add link
+      </Button>
+
     </React.Fragment>
   )
 }
