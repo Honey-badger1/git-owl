0d5eb31b94fa1dc4e05a39c186a0e26fc4639184
Margo Astreyko <margoastreyko@mail.ru>
2020-11-16 19:58:38 +0300

    changed travis files

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo "###### Starting Deployment ######"
+
+scp -r client/dist/* root@$IP:$DEPLOY_DIR/client/
+scp -r backend/* root@$IP:$DEPLOY_DIR
+
+echo "###### Continue Deployment ######"
+
+ssh root@$IP <<EOF
+ cd $DEPLOY_DIR
+ npm i
+ pm2 restart owl
+EOF
+
+echo "###### End Deployment ######"
