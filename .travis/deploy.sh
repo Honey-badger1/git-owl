4fc9414063615a1b5b5a346d55dec405fe154cba
Margo Astreyko <margoastreyko@mail.ru>
2020-11-17 03:50:04 +0300

    Testing travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-echo "###### Starting Deployment ######"
-
-scp -r client/dist/* root@$IP:$DEPLOY_DIR/client/
-scp -r backend/* root@$IP:$DEPLOY_DIR
-
-echo "###### Continue Deployment ######"
-
-ssh root@$IP <<EOF
- cd $DEPLOY_DIR
- npm i
- pm2 restart owl
-EOF
-
-echo "###### End Deployment ######"
+set -e
+ssh root@165.225.158.125 -p -v exit
